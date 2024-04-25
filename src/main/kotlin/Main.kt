a6ef5cb9d1cfd3647409964a47a0e9fd957b2362
Leonardo Toloza <leo_eltolo@hotmail.com>
2024-04-25 09:43:29 -0300

    haciendo menu principal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,27 @@
-import repositories.UserRepository.isUserValid
+import java.awt.Menu
 
 fun main(args: Array<String>) {
 
     val userLogged = launchLogin()
 
+    var optionSelected : Int
+    do {
+        println("---- Menú ----")
+        println("1. Comprar")
+        println("2. Carrito")
+        println("3. Historial de compra")
+        println("4. Cerrar Programa")
+        print("Ingrese su opción: ")
+
+        optionSelected = readln().toIntOrNull()?:0
+
+        when(optionSelected){
+//            1 -> goToShoppingMenu()
+//            2 -> showOrderPurchasesList()
+//            3 -> showCart()
+            4 -> break
+        }
+
+    } while (true)
 }
 
